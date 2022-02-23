1970e4d222f92ac0bbf609f5f14dd8b7a66e5341
cbedroid <cbedroid1614@gmail.com>
2022-02-22 22:15:47 -0500

    Added redux method to handle map coordinance

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,23 +1,40 @@
-import React from 'react'
-import { StyleSheet, Text, View, SafeAreaView, Image } from 'react-native'
+import React from "react"
+import { useDispatch } from "react-redux"
+import { View, SafeAreaView, Image } from "react-native"
 import tw from "tailwind-react-native-classnames"
-import NavOptions from '../components/NavOptions';
-import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete';
-// https://github.com/FaridSafi/react-native-google-places-autocomplete
+import NavOptions from "../components/NavOptions";
+import { GooglePlacesAutocomplete } from "react-native-google-places-autocomplete"; // https://github.com/FaridSafi/react-native-google-places-autocomplete
+import { setDestination, setOrigin } from "../store/slices/navSlice"
 
-import { GOOGLE_MAPS_APIKEY } from '@env'
+// eslint-disable-next-line import/no-unresolved
+import { GOOGLE_MAPS_APIKEY } from "@env"
 
 
 const HomeScreen = () => {
+  const dispatch = useDispatch()
+
+
+  const handleOnPress = (data, details = null) => {
+    console.log("data", data, "\ndetails", details)
+    // get user input, call google api, save location
+    dispatch(setOrigin(
+      {
+        location: details.geometry.location,
+        description: data.description
+      }
+    ))
+    // reset destination for new data 
+    dispatch(setDestination(null))
+  }
+
   return (
     <SafeAreaView style={tw`bg-white h-full`}>
       <View style={tw`p-5`}>
         <Image
-          style={{ width: 100, height: 100, resizeMode: 'contain' }}
+          style={{ width: 100, height: 100, resizeMode: "contain" }}
           source={{ uri: "https://links.papareact.com/gzs" }}
         />
         <GooglePlacesAutocomplete
-
           enablePoweredByContainer={false}
           minLength={2}
           placeholder="Where From?"
@@ -26,16 +43,14 @@ const HomeScreen = () => {
           fetchDetails={true}
           returnKeyType={"search"}
 
-          onPress={(data, details = null) => {
-            console.log("data", data, "\ndetails", details)
-          }}
+          onPress={(data, details = null) => handleOnPress(data, details)}
           onFail={(e) => console.log("Error", e)}
           onNotFound={(nf) => console.log("Not Found", nf)}
 
           query={{ key: GOOGLE_MAPS_APIKEY, language: "en" }}
           styles={{
             container: { flex: 0 },
-            textInput: { fontSize: 18 }
+            textInput: { fontSize: 18, fontWeight: "600" }
           }}
         />
         <NavOptions />
@@ -45,6 +60,3 @@ const HomeScreen = () => {
 }
 
 export default HomeScreen
-
-const styles = StyleSheet.create({
-})
