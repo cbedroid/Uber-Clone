306949b83fceade7ce808955861dbd1d0f90da6b
cbedroid <cbedroid1614@gmail.com>
2022-02-22 20:18:18 -0500

    Added NavigationCard and RideOptions components

---
+++ components/RideOptionsCard.js
@@ -0,0 +1,16 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import tw from 'tailwind-react-native-classnames'
+
+const NavigateCard = () => {
+  return (
+    <View style={tw`text-black`}>
+      <Text>NavigateCard</Text>
+      <Text>Hello World</Text>
+    </View>
+  )
+}
+
+export default NavigateCard
+
+const styles = StyleSheet.create({})
