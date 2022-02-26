814b11e8a309f5cd1e61d3cf3ba7fa8e8d533a06
cbedroid <cbedroid1614@gmail.com>
2022-02-26 02:10:55 -0500

    Added Maps and Eats screens

---
+++ src/screens/MapScreen.js
@@ -0,0 +1,39 @@
+import React from "react"
+import { createStackNavigator } from "@react-navigation/stack"
+import { View } from "react-native"
+import tw from "tailwind-react-native-classnames"
+import Maps from "../components/Maps"
+import NavigateCard from "../components/NavigateCard"
+import RideOptionsCard from "../components/RideOptionsCard"
+
+
+const MapScreen = () => {
+  const Stack = createStackNavigator()
+
+
+  return (
+    <View>
+      <View style={tw`h-1/2`}>
+        <Maps />
+      </View>
+      <View style={tw`h-1/2 bg-black`}>
+        <Stack.Navigator>
+          <Stack.Screen
+            name="NavigateCard"
+            component={NavigateCard}
+            options={{ headerShown: false }}
+          />
+
+          <Stack.Screen
+            name="RideOptionsCard"
+            component={RideOptionsCard}
+            options={{ headerShown: false }}
+          />
+
+        </Stack.Navigator>
+      </View>
+    </View>
+  )
+}
+
+export default MapScreen
