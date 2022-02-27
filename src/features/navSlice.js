d7deee0efc1b54d90317f836aa860ef6de55c5f0
cbedroid <cbedroid1614@gmail.com>
2022-02-27 06:20:31 -0500

    Removed redux initial dummy data, added more reducers

--- src/features/navSlice.js
+++ src/features/navSlice.js
@@ -1,17 +1,11 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-  origin: {
-    description: "San Francisco,California",
-    location: {
-      lat: 37.78825,
-      lng: -122.4324,
-      latitudeDelta: 0.0922,
-      longitudeDelta: 0.0421,
-    }
-  },
+  origin: null,
   destination: null,
-  travelTimeInformation: null
+  travelTimeInformation: null,
+  directions: [],
+  coordinates: [],
 }
 
 
@@ -24,7 +18,6 @@ export const navSlice = createSlice({
     setOrigin: (state, action) => {
       state.origin = action.payload;
     },
-
     setDestination: (state, action) => {
       state.destination = action.payload;
     },
@@ -32,15 +25,23 @@ export const navSlice = createSlice({
     setTravelTimeInformation: (state, action) => {
       state.travelTimeInformation = action.payload;
     },
+    setDirections: (state, action) => {
+      state.directions = action.payload;
+    },
+    setCoordinates: (state, action) => {
+      state.coordinates = action.payload;
+    },
   },
 })
 
 /// export nav actions
-export const { setOrigin, setDestination, setTravelTimeInformation } = navSlice.actions;
+export const { setOrigin, setDestination, setTravelTimeInformation, setDirections, setCoordinates } = navSlice.actions;
 
 // export Selectors
 export const selectOrigin = (state) => state.nav.origin;
 export const selectDestination = (state) => state.nav.destination;
+export const selectDirections = (state) => state.nav.directions;
+export const selectCoordinates = (state) => state.nav.coordinates;
 export const selectTravelTimeInformation = (state) => state.nav.travelTimeInformation;
 
 // export navReducer
