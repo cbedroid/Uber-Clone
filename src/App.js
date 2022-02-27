a99f270046ffb0c152b673d07b84a8dbac22c8d2
cbedroid <cbedroid1614@gmail.com>
2022-02-27 06:18:08 -0500

    Sorted import, Added KeyboardAvoidView and navigator links component

--- src/App.js
+++ src/App.js
@@ -1,30 +1,46 @@
 import React from "react";
-import HomeScreen from "./screens/HomeScreen";
-import MapScreen from "./screens/MapScreen";
-import EatsScreen from "./screens/EatsScreen";
-import { SafeAreaProvider } from "react-native-safe-area-context";
+import { KeyboardAvoidingView, Platform } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
+import { SafeAreaProvider } from "react-native-safe-area-context";
 import "react-native-gesture-handler";
-import { store } from "./store/index"
-import { Provider } from "react-redux"
-
+import { Provider } from "react-redux";
+import EatsScreen from "./screens/EatsScreen";
+import HomeScreen from "./screens/HomeScreen";
+import MapScreen from "./screens/MapScreen";
+import { store } from "./store/index";
 
 export default function App() {
   const Stack = createStackNavigator();
-
   return (
     <Provider store={store}>
       <NavigationContainer>
         <SafeAreaProvider>
-          <Stack.Navigator>
-            <Stack.Screen name="HomeScreen" component={HomeScreen} options={{ headerShown: false, }} />
-            <Stack.Screen name="MapScreen" component={MapScreen} options={{ headerShown: false, }} />
-            <Stack.Screen name="EatsScreen" component={EatsScreen} options={{ headerShown: false, }} />
-          </Stack.Navigator>
+          <KeyboardAvoidingView
+            behavior={Platform.OS === "ios" ? "padding" : "height"}
+            keyboardVerticalOffset={Platform.OS === "ios" ? -64 : 0}
+            style={{ flex: 1 }}
+          >
+            <Stack.Navigator>
+              <Stack.Screen
+                name="HomeScreen"
+                component={HomeScreen}
+                options={{ headerShown: false }}
+              />
+              <Stack.Screen
+                name="MapScreen"
+                component={MapScreen}
+                options={{ headerShown: false }}
+              />
+              <Stack.Screen
+                name="EatsScreen"
+                component={EatsScreen}
+                options={{ headerShown: false }}
+              />
+            </Stack.Navigator>
+          </KeyboardAvoidingView>
         </SafeAreaProvider>
       </NavigationContainer>
     </Provider>
   );
 }
-
