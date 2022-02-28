d037261bdfb0d8982f2b37de754aa3cd5c3631de
cbedroid <cbedroid1614@gmail.com>
2022-02-28 01:21:55 -0500

    Added floating menu to maps

--- src/screens/MapScreen.js
+++ src/screens/MapScreen.js
@@ -1,6 +1,8 @@
 import React from "react";
-import { View } from "react-native";
+import { TouchableOpacity, View } from "react-native";
+import { useNavigation } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
+import { Icon } from "react-native-elements";
 import tw from "tailwind-react-native-classnames";
 import Maps from "../components/Maps";
 import NavigateCard from "../components/NavigateCard";
@@ -8,9 +10,16 @@ import RideOptionsCard from "../components/RideOptionsCard";
 
 const MapScreen = () => {
   const Stack = createStackNavigator();
+  const navigation = useNavigation();
 
   return (
     <View>
+      <TouchableOpacity
+        onPress={() => navigation.navigate("HomeScreen")}
+        style={tw`bg-gray-200 absolute top-16 left-8 z-50 p-2 rounded-full shadow-lg`}
+      >
+        <Icon name="home" />
+      </TouchableOpacity>
       <View style={tw`h-1/2`}>
         <Maps />
       </View>
