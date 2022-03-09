4a3b028200891138deccc1fe8df5a4f6083066b5
cbedroid <cbedroid1614@gmail.com>
2022-03-09 09:40:03 -0500

    Fixed Virtual List issue, Added Flatlist components

--- src/components/RideOptionsCard.js
+++ src/components/RideOptionsCard.js
@@ -3,19 +3,31 @@ import "intl/locale-data/jsonp/en";
 
 import React, { useEffect, useState } from "react";
 import { FlatList, Image, SafeAreaView, Text, TouchableOpacity, View } from "react-native";
-import { useNavigation, CommonActions } from "@react-navigation/native";
+import axios from "axios";
+import moment from "moment";
 import { Icon } from "react-native-elements";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import tw from "tailwind-react-native-classnames";
+import { setDriver, selectDriver } from "../features/driverSlice";
 import { selectTravelTimeInformation } from "../features/navSlice";
+const { faker } = require("@faker-js/faker");
 const UberLux = require("../assets/uber_lux_car.webp");
 const UberX = require("../assets/uber_x_car.webp");
 const UberXL = require("../assets/uber_xl_car.webp");
 
+const uberFares = {
+  baseFare: 749,
+  perMin: 28,
+  perMile: 92,
+  surCharge: 100,
+  longPickupFee: 121,
+  bookingFee: 260,
+};
 const data = [
   {
     id: "Uber-X-123",
     title: "UberX",
+    passengers: 3,
     points: 1,
     multiplier: 1,
     image: UberX,
@@ -23,6 +35,7 @@ const data = [
   {
     id: "Uber-XL-456",
     title: "Uber XL",
+    passengers: 5,
     points: 2,
     multiplier: 1.2,
     image: UberXL,
@@ -30,14 +43,24 @@ const data = [
   {
     id: "Uber-LUX-789",
     title: "Uber LUX",
+    passengers: 3,
     points: 3,
     multiplier: 1.75,
     image: UberLux,
   },
 ];
 
-const NavigateCard = () => {
-  const navigation = useNavigation();
+const driverData = () => {
+  return {
+    name: faker.name.firstName(),
+    avatar: faker.image.avatar(),
+    vehicle: faker.fake("{{vehicle.color}} {{vehicle.vehicle}}"),
+    license_plate: faker.vehicle.vrm(),
+    rating: (Math.random() * 2 + 3).toFixed(2),
+  };
+};
+
+const RideOptionCard = ({ navigation: { navigate } }) => {
   const [selected, setSelectedItem] = useState(null);
   const [paymentRates, setPaymentRates] = useState(null);
   const [currentRate, setCurrentRate] = useState({});
@@ -46,15 +69,43 @@ const NavigateCard = () => {
 
   // Surge charge base on the time of the day, events, holiday ..etc
   // TODO: Added surge charge to day,night, and holidays
-  const SURGE_CHARGE_RATE = 1.5;
+  // const SURGE_CHARGE_RATE = 1.5;
+
+  const dispatch = useDispatch();
+  const driver = useSelector(selectDriver);
+
+  const getDriverInfo = async () => {
+    //DOC: https://carsapi1.docs.apiary.io/#reference/0/cars-collection/
+    // TODO: Move api to redux to prevent recall api
+    const url = "https://private-anon-c49f61dfdb-carsapi1.apiary-mock.com/cars";
+    const response = await axios({
+      baseURL: url,
+      method: "GET",
+      responseType: "json",
+    });
+
+    if (!response.data) return;
+    let car = [];
+    while (car.length === 0) {
+      const [make, model] = faker.vehicle.vehicle().split(" ");
+      car = response.data.filter((car) => {
+        return car.make === make.toLowerCase() && car.model === model.toLowerCase();
+      });
+    }
+
+    dispatch(
+      setDriver({
+        ...driverData(),
+        ...{ vehicle: { ...car[0], color: faker.vehicle.color() } },
+      })
+    );
+  };
 
   useEffect(() => {
-    console.log("RideOption loaded", travelTimeInfo);
     // setPaymentRates(null);
-    console.log("\n********************************************************************************");
+    console.log("RideOptionCard loaded");
     const initial_rates = data.map((item) => calculateInternationalRate(item));
     setPaymentRates(initial_rates);
-    console.log("\n\nRideRate", initial_rates);
   }, [travelTimeInfo]);
 
   const handleTravelTime = (duration) => {
@@ -65,82 +116,105 @@ const NavigateCard = () => {
 
   const calculateInternationalRate = ({ id, multiplier }) => {
     if (!travelTimeInfo) return;
+    // apply Uber base fare charges to Ride
+    let currentFare =
+      uberFares.baseFare + (travelTimeInfo.time / 60) * uberFares.perMin + Math.floor(+travelTimeInfo.distance) * uberFares.perMile + uberFares.bookingFee;
+
+    console.log("\nCost", currentFare, travelTimeInfo.duration);
     const rate = new Intl.NumberFormat("en-us", {
       style: "currency",
       currency: "USD",
-    }).format((+travelTimeInfo?.time * SURGE_CHARGE_RATE * multiplier) / 100);
+    }).format((currentFare * multiplier) / 100);
 
     const [dollars, cents] = rate.toString().split(".");
     const currentRates = paymentRates || {};
+
+    // break down fare into a price categories
     currentRates[id] = {
       id: id,
       dollars: dollars,
       cents: cents,
       real_price: rate,
     };
-    console.log("\nRate for ", id, rate, travelTimeInfo?.realTime);
     return currentRates[id];
   };
 
   const handleSelect = (item) => {
+    if (item === selected) return;
     setSelectedItem(item);
     if (!paymentRates) return;
-    console.log("Setting Current Rate");
     const payRate = paymentRates[data.indexOf(item)];
     setCurrentRate(payRate);
+    getDriverInfo();
   };
 
-  return (
-    <SafeAreaView style={tw`bg-white flex-grow`}>
-      <View style={tw`text-black`}>
-        <TouchableOpacity
-          style={tw`absolute top-3 left-5 p-3 z-50 rounded-full`}
-          onPress={() => navigation.navigate("NavigateCard")}
-        >
-          <Icon name="chevron-left" type="font-awesome" />
-        </TouchableOpacity>
-        <Text style={tw`text-center text-xl font-bold py-5`}>
-          Select a Ride -{" "}
-          <Text style={tw`font-normal`}>
-            {travelTimeInfo?.distance.toFixed(2)} {travelTimeInfo && "mi"}
-          </Text>
-        </Text>
-      </View>
-
-      <FlatList
-        data={data}
-        keyExtractor={(item) => item.id}
-        renderItem={({ item: { id, title, multiplier, image }, item, index }) => (
-          <TouchableOpacity
-            style={tw`flex-row items-center justify-between px-4  ${id === selected?.id && "bg-gray-200"}`}
-            onPress={() => handleSelect(item)}
-          >
+  const carOptionList = React.useCallback(
+    ({ item: { id, title, image }, item, index }) => {
+      return (
+        <View>
+          <TouchableOpacity style={tw`flex-row items-center justify-between px-4  ${id === selected?.id && "bg-gray-200"}`} onPress={() => handleSelect(item)}>
             <Image style={{ width: 100, height: 100, resizeMode: "contain" }} source={image} />
             <View style={tw`-ml-3 flex-1`}>
-              <Text style={tw`text-xl font-bold`}>{title}</Text>
-              <Text style={tw`font-normal w-48`}>{handleTravelTime(travelTimeInfo?.formattedTime)} Travel Time...</Text>
+              <View style={tw`flex flex-row items-end`}>
+                <Text style={[tw`text-xl mr-1`, { fontFamily: "UberMoveMedium" }]}>{title}</Text>
+                {id === selected?.id && (
+                  <>
+                    <Icon style={tw`self-end p-1 pr-0`} name="user" type="font-awesome" size={14} />
+                    <Text style={[{ lineHeight: 32 }]}> {item.passengers}</Text>
+                  </>
+                )}
+              </View>
+
+              <Text style={[tw`text-xs text-gray-600`, { fontFamily: "UberTextRegular" }]}>
+                {moment().add(travelTimeInfo?.time, "seconds").format("hh:mm A")} {id === selected?.id && "dropoff"}
+              </Text>
+              {id === selected?.id && (
+                <Text style={[tw`text-xs text-gray-600`, { fontFamily: "UberTextRegular" }]}>
+                  {handleTravelTime(travelTimeInfo?.formattedTime)} travel time...
+                </Text>
+              )}
             </View>
             <View>
               {paymentRates && (
                 <View style={tw`flex flex-row `}>
                   <Text style={tw`text-xl font-bold`}>{paymentRates[index]?.dollars}</Text>
-                  <Text style={{ fontSize: 14, lineHeight: 24 }}>{paymentRates[index]?.cents}</Text>
+                  <Text style={{ fontSize: 14, lineHeight: 22 }}>{paymentRates[index]?.cents}</Text>
                 </View>
               )}
               <Text style={tw`text-gray-400 font-semibold`}>{item?.points}x pts</Text>
             </View>
           </TouchableOpacity>
-        )}
-      />
+        </View>
+      );
+    },
+    [selected, paymentRates]
+  );
+
+  const listHeader = React.useCallback(() => {
+    return (
+      <View style={tw`text-black`}>
+        <TouchableOpacity style={tw`absolute top-3 left-5 p-3 z-50 rounded-full`} onPress={() => navigate("RideOptionsCard")}>
+          <Icon name="chevron-left" type="font-awesome" />
+        </TouchableOpacity>
+        <Text style={tw`text-center text-gray-500  py-5`}>
+          Choose a ride -{""}
+          <Text style={tw`font-normal`}>
+            {travelTimeInfo?.distance.toFixed(2)} {travelTimeInfo && "mi"}
+          </Text>
+        </Text>
+      </View>
+    );
+  }, []);
+
+  const selectRideButton = React.useCallback(() => {
+    return (
       <View style={tw`border-t border-gray-200 pt-1 mt-auto`}>
         <TouchableOpacity
           onPress={() =>
-            navigation.dispatch(
-              CommonActions.navigate({
-                name: "ConfirmScreen",
-                params: { currentRate: currentRate?.real_price },
-              })
-            )
+            navigate({
+              name: "ConfirmPaymentCard",
+              params: { currentRate: currentRate?.real_price, ride: data.filter((ride) => ride.id === selected?.id) },
+            })
           }
           style={tw`bg-black py-3 mx-3 ${!selected && "bg-gray-300"}`}
           disabled={!selected}
@@ -152,8 +226,20 @@ const NavigateCard = () => {
           </Text>
         </TouchableOpacity>
       </View>
+    );
+  }, [selected]);
+
+  return (
+    <SafeAreaView style={tw`bg-white flex-grow`}>
+      <FlatList
+        data={data}
+        keyExtractor={(item) => item.id}
+        renderItem={carOptionList}
+        ListHeaderComponent={listHeader}
+        ListFooterComponent={selectRideButton}
+      />
     </SafeAreaView>
   );
 };
 
-export default NavigateCard;
+export default RideOptionCard;
