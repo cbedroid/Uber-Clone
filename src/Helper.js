59814be9eb12da0d7d473e801a0629d6525d95f1
cbedroid <cbedroid1614@gmail.com>
2022-03-11 02:59:49 -0500

    Updated helper - Added API methods

--- src/Helper.js
+++ src/Helper.js
@@ -1,8 +1,15 @@
 import { Dimensions } from "react-native";
+// eslint-disable-next-line import/no-unresolved
+import { MAPQUEST_APIKEY } from "@env";
+import axios from "axios";
 
+export const sleep = (milliseconds) => {
+  return new Promise((resolve) => setTimeout(resolve, milliseconds));
+};
 export const textEllipsis = (text, offset = 120) => {
   // Custom text ellipsis style for text input
 
+  if (!text) return text;
   const window_width = Dimensions.get("screen").width - offset;
   text = text.trim();
   const CHAR_SIZE = 8; // font size
@@ -16,4 +23,45 @@ export const toTitleCase = (...str) => {
   return str.replace(/\w\S*/g, function (txt) {
     return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
   });
+};
+export const fetchPlacesApi = async (search) => {
+  if (!search) return;
+  const mapQuestURL = "https://web.mapquestapi.com/search/v3/prediction";
+  try {
+    const api_resp = await axios({
+      baseURL: mapQuestURL,
+      method: "GET",
+      params: {
+        collection: "address,adminArea,airport,category,franchise,poi",
+        feedback: true,
+        key: MAPQUEST_APIKEY,
+        limit: 15,
+        q: search,
+      },
+      responseType: "json",
+    });
+    return await api_resp;
+  } catch (error) {
+    // setErrorMsg("Sorry, we could not find this place!");
+    console.error("Error", error);
+  }
+};
+export const fetchDirectionalApi = async (origin, destination = {}) => {
+  const mapQuestDirectionalUrl = "http://www.mapquestapi.com/directions/v2/route";
+  try {
+    const api_resp = await axios({
+      baseURL: mapQuestDirectionalUrl,
+      method: "GET",
+      params: {
+        to: origin,
+        from: destination,
+        unit: "m",
+        key: MAPQUEST_APIKEY,
+      },
+      responseType: "json",
+    });
+    return api_resp;
+  } catch (error) {
+    console.error("Directional Api Error", error);
+  }
 };
