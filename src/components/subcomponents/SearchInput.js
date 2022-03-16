d0e156a98df17c9493babea9ec5ab5f8f65e22e5
cbedroid <cbedroid1614@gmail.com>
2022-03-16 03:11:41 -0400

    Fixed inputs layout and styles

--- src/components/subcomponents/SearchInput.js
+++ src/components/subcomponents/SearchInput.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { View, TextInput } from "react-native";
 import { useNavigation } from "@react-navigation/native";
 import { PropTypes } from "prop-types";
@@ -8,6 +8,8 @@ import tw from "tailwind-react-native-classnames";
 import { selectUserLocation } from "../../features/locationSlice";
 import { setOrigin, setDestination, selectDestination } from "../../features/navSlice";
 import { fetchPlacesApi } from "../../Helper";
+import { textEllipsis } from "../../Helper";
+import AnimatedIcon from "./AnimatedIcon";
 const _ = require("lodash");
 
 // eslint-disable-next-line react/display-name
@@ -27,19 +29,40 @@ const SearchInput = React.forwardRef(({ handleData }, ref) => {
   const [loading, setLoading] = useState(null);
   const [apiResults, setApiResults] = useState(null);
 
+  useEffect(() => {
+    console.log("Search Loading ");
+    setInputValue_2(null);
+  }, []);
+
   /* Dispatch origin or destination to store*/
   React.useImperativeHandle(ref, () => ({
     setOriginDestination(item) {
       if (!apiResults) return;
-      const action = focusedInput === 0 ? setOrigin : setDestination;
+
+      let action;
+      const value = item.displayString;
+
+      // set dispatch action and input value based on to the focused input
+      if (focusedInput === 0) {
+        action = setOrigin;
+        setInputValue_1(value);
+      } else {
+        action = setDestination;
+        setInputValue_2(value);
+      }
 
       dispatch(action(item));
       //TODO: Navigate if destination is set, then navigate to MapScreen
-      if (destination) navigation.navigate("MapScreen");
+      // only move to next screen if the second input has a value
+      if (inputValue_2 && destination) navigation.navigate("MapScreen");
     },
   }));
+
   const handleTextChange = async (input) => {
-    input = input.toLowerCase().trim();
+    // set the current input value
+    focusedInput === 0 ? setInputValue_1(input) : setInputValue_2(input);
+
+    input = input.toLowerCase().trim(); //clean inputs
     if (input.length < 6) return;
 
     // Prevents recalling fetchData when user is either backspacing
@@ -70,7 +93,6 @@ const SearchInput = React.forwardRef(({ handleData }, ref) => {
       return { ...result, displayString };
     });
   };
-
   return (
     <View style={tw`p-0`}>
       <View style={tw`flex flex-row px-4 items-center drop-shadow`}>
@@ -80,28 +102,35 @@ const SearchInput = React.forwardRef(({ handleData }, ref) => {
           <View style={tw`${focusedInput === 0 ? "bg-gray-400" : "bg-gray-700"}  h-2 w-2`}></View>
         </View>
         <View style={tw`flex-1`}>
-          <TextInput
-            ref={inputRef_1}
-            value={inputValue_1}
-            onChangeText={handleTextChange}
-            blurOnSubmit={false}
-            placeholder={userLocation?.street || "Enter pickup location"}
-            searchIcon={false}
-            onFocus={() => setFocusedInput(0)}
-            style={tw`${focusedInput === 0 ? "bg-gray-100" : "bg-gray-50"} text-xl ${inputValue_1 ? "text-gray-600" : "italic"} m-2 p-2`}
-          />
-          <TextInput
-            ref={inputRef_2}
-            value={inputValue_2}
-            onChangeText={handleTextChange}
-            searchIcon={false}
-            onFocus={() => setFocusedInput(1)}
-            blurOnSubmit={false}
-            placeholder="Where to?"
-            containerStyle={tw`bg-transparent border-0`}
-            inputContainerStyle={tw`${focusedInput === 1 ? "bg-gray-100" : "bg-gray-50"} px-2`}
-            style={tw`${focusedInput === 1 ? "bg-gray-100" : "bg-gray-50"} text-xl ${inputValue_2 ? "text-gray-600" : "italic"} p-2 m-2 mb-0`}
-          />
+          <View style={tw`flex flex-row items-center content-between ${focusedInput === 0 ? "bg-gray-100" : "bg-gray-50"}  p-2 m-2 mb-0 `}>
+            <TextInput
+              ref={inputRef_1}
+              value={textEllipsis(inputValue_1)}
+              onChangeText={handleTextChange}
+              blurOnSubmit={false}
+              placeholder={userLocation?.street || "Enter pickup location"}
+              searchIcon={false}
+              onFocus={() => setFocusedInput(0)}
+              inputContainerStyle={tw` px-2`}
+              style={tw`flex-1 ${focusedInput === 0 ? "bg-gray-100" : "bg-gray-50"} text-xl ${inputValue_1 ? "text-gray-600" : "italic"}`}
+            />
+            <AnimatedIcon isHidden={!inputValue_1} handleClearInput={() => setInputValue_1(null)} type="font-awesome" name="close" size={16} color="#c3c3c3" />
+          </View>
+          <View style={tw`flex flex-row items-center content-between ${focusedInput === 1 ? "bg-gray-100" : "bg-gray-50"}  p-2 m-2 mb-0 `}>
+            <TextInput
+              ref={inputRef_2}
+              value={textEllipsis(inputValue_2)}
+              onChangeText={handleTextChange}
+              searchIcon={false}
+              onFocus={() => setFocusedInput(1)}
+              blurOnSubmit={false}
+              placeholder="Where to?"
+              containerStyle={tw`bg-transparent border-0`}
+              inputContainerStyle={tw` px-2`}
+              style={tw`flex-1 ${focusedInput === 1 ? "bg-gray-100" : "bg-gray-50"} text-xl ${inputValue_2 ? "text-gray-600" : "italic"} `}
+            />
+            <AnimatedIcon isHidden={!inputValue_2} handleClearInput={() => setInputValue_2(null)} type="font-awesome" name="close" size={16} color="#c3c3c3" />
+          </View>
         </View>
         <FAB icon={{ name: "add", color: "#000" }} size="small" color="#f3f3f3" visible={true} />
       </View>
