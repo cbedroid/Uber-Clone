b5fa03d360f0b2b08e7fba56f79c64feac861f03
cbedroid <cbedroid1614@gmail.com>
2022-03-16 03:15:58 -0400

    Removed user location method to Loading Screen

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,26 +1,17 @@
 import React, { useEffect } from "react";
 import { View, SafeAreaView, Image, Text, ScrollView } from "react-native";
-import { Platform } from "react-native";
 import { Keyboard } from "react-native";
 // eslint-disable-next-line import/no-unresolved
-import { MAPQUEST_APIKEY } from "@env";
-import axios from "axios";
-import Constants from "expo-constants";
-import * as Location from "expo-location";
 import { Button, FAB } from "react-native-elements";
-import { useDispatch, useSelector } from "react-redux";
 import tw from "tailwind-react-native-classnames";
 import NavFavourites from "../components/NavFavourites";
 import NavOptions from "../components/NavOptions";
 import SearchButton from "../components/subcomponents/SearchButton";
-import { setNearbyPlaces, selectNearbyPlaces, setUserLocation } from "../features/locationSlice";
 // eslint-disable-next-line import/no-unresolved
 const _ = require("lodash");
 const UberRideCar = require("../assets/uber_x_icon_nbg_2.webp");
 
 const HomeScreen = () => {
-  const dispatch = useDispatch();
-  const nearbyPlaces = useSelector(selectNearbyPlaces);
   const dropDownRef = React.createRef();
   const scrollRef = React.createRef();
 
@@ -34,73 +25,6 @@ const HomeScreen = () => {
       scrollRef.current.scrollTo({ y: 0, animate: true });
     });
   });
-
-  useEffect(() => {
-    console.log("HomeScreen loaded");
-
-    getUserLocation();
-  }, []);
-
-  const getUserLocation = async () => {
-    /* @hide */
-    if (Platform.OS === "android" && !Constants.isDevice) {
-      alert("Oops, this will not work on Snack in an Android emulator. Try it on your device!");
-      return;
-    }
-    /* @end */
-    let { status } = await Location.requestForegroundPermissionsAsync();
-    if (status !== "granted") {
-      alert("Permission to access location was denied");
-      return;
-    }
-    const location = await Location.getCurrentPositionAsync({});
-
-    if (!_.isEmpty(nearbyPlaces)) return;
-    getNearbyPlaces(location);
-    fetchUserAddress(location);
-  };
-
-  const getNearbyPlaces = async (location) => {
-    const lat = location?.coords?.latitude;
-    const lon = location?.coords?.longitude;
-
-    const baseURL = "http://www.mapquestapi.com/search/v2/radius";
-
-    const api_resp = await axios({
-      baseURL: baseURL,
-      method: "GET",
-      params: {
-        maxMatches: 10,
-        origin: `${lat},${lon}`,
-        key: MAPQUEST_APIKEY,
-      },
-      responseType: "json",
-    });
-
-    if (api_resp.status !== 200) return;
-    dispatch(setNearbyPlaces(api_resp.data.searchResults));
-  };
-
-  const fetchUserAddress = async (location) => {
-    console.log("Fetching User Address ");
-    const lat = location?.coords?.latitude;
-    const lon = location?.coords?.longitude;
-    const baseURL = "http://www.mapquestapi.com/geocoding/v1/reverse";
-
-    const api_resp = await axios({
-      baseURL: baseURL,
-      method: "GET",
-      params: {
-        location: `${lat},${lon}`,
-        key: MAPQUEST_APIKEY,
-      },
-      responseType: "json",
-    });
-
-    if (api_resp.status !== 200) return;
-
-    dispatch(setUserLocation(api_resp.data.results[0].locations[0]));
-  };
 
   return (
     <SafeAreaView style={tw`bg-white h-full`}>
@@ -126,10 +50,9 @@ const HomeScreen = () => {
           >
             <View style={tw`h-24`}>
               <FAB visible={true} icon={{ name: "menu", type: "fontawesome" }} size="small" style={tw`absolute top-8 left-0 `} color="#EFF1F2" />
-              {/* <Image style={{ width: 75, height: 100, resizeMode: "contain" }} source={UberLogo} /> */}
             </View>
             <View>
-              <Text style={tw`text-2xl font-bold text-white my-2`}>See you There</Text>
+              <Text style={tw`text-2xl font-bold text-white my-2`}>See You There</Text>
               <Text style={tw`text-white font-light`}>Travel soon? Request pickup at any hour</Text>
               <Button
                 title="Book Now"
