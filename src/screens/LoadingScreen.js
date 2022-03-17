7ac3902bcc56e94ac13c849d6d349a60ababcf67
cbedroid <cbedroid1614@gmail.com>
2022-03-17 03:06:06 -0400

    Moved Location request medthod to utils file

--- src/screens/LoadingScreen.js
+++ src/screens/LoadingScreen.js
@@ -1,16 +1,10 @@
 import React, { useEffect, useRef, useState } from "react";
-import { View, Animated, Easing, Platform, Image } from "react-native";
-import Constants from "expo-constants";
-import * as Location from "expo-location";
-import { useDispatch } from "react-redux";
+import { View, Animated, Easing, Image } from "react-native";
 import tw from "tailwind-react-native-classnames";
-import { setUserLocation, setNearbyPlaces } from "../features/locationSlice";
-import { fetchNearbyPlaces, fetchUserAddress } from "../Helper";
-const _ = require("lodash");
+import { requestUserLocationPermission } from "../Utils";
 const UberLogo = require("../assets/uber_logo_white.webp");
 
 const LoadingScreen = ({ navigation }) => {
-  const dispatch = useDispatch();
   const [loading, setLoading] = useState(true);
   const scaleAnimation = useRef(new Animated.Value(0)).current; // Initial value for opacity: 0
 
@@ -23,50 +17,15 @@ const LoadingScreen = ({ navigation }) => {
     }).start();
   });
 
-  useEffect(() => {
+  useEffect(async () => {
     console.log("App loading");
-    requestUserLocationPermission();
+    await requestUserLocationPermission();
+    setLoading(false);
   }, []);
 
   useEffect(() => {
     if (!loading) navigation.navigate("HomeScreen");
   }, [loading]);
-
-  const requestUserLocationPermission = async () => {
-    /* @hide */
-    if (Platform.OS === "android" && !Constants.isDevice) {
-      alert("Oops, this will not work on Snack in an Android emulator. Try it on your device!");
-      return;
-    }
-    /* @end */
-    let { status } = await Location.requestForegroundPermissionsAsync();
-    if (status !== "granted") {
-      alert("Permission to access location was denied");
-      return;
-    }
-
-    // Requesting user location permissions
-    let location = null;
-    try {
-      console.log("\nRequesting user location permissions ...");
-      location = await Location.getCurrentPositionAsync({ accuracy: 5 });
-    } catch {
-      console.log("\nUserLocationRequest: denied");
-    }
-
-    // If location permission is granted, then get user's current location
-    if (!_.isEmpty(location)) {
-      console.log("\nFetching user location");
-      const nearbyPlacesResponse = await fetchNearbyPlaces(location);
-      const userAddressResponse = await fetchUserAddress(location);
-
-      if (nearbyPlacesResponse.status === 200 || userAddressResponse.status === 200) {
-        dispatch(setNearbyPlaces(nearbyPlacesResponse.data.searchResults));
-        dispatch(setUserLocation(userAddressResponse.data.results[0].locations[0]));
-      }
-    }
-    setLoading(false);
-  };
 
   return (
     <Animated.View
