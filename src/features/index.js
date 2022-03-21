a59bcff71a75b681b5a00783676248bb40956128
cbedroid <cbedroid1614@gmail.com>
2022-03-21 01:36:58 -0400

    Added UserSlice, fixed Reducers naming

--- src/features/index.js
+++ src/features/index.js
@@ -1,10 +1,12 @@
 import { combineReducers } from "redux";
 import locationReducer from "./locationSlice";
 import navReducer from "./navSlice";
-import rideSlice from "./rideSlice";
+import rideReducer from "./rideSlice";
+import userReducer from "./userSlice";
 
 export default combineReducers({
   nav: navReducer,
-  ride: rideSlice,
+  ride: rideReducer,
   location: locationReducer,
+  user: userReducer,
 });
---
+++ src/features/userSlice.js
@@ -0,0 +1,29 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  user: {
+    firstName: null,
+    lastName: null,
+    phoneNumber: null,
+    phoneNumberVerified: false,
+  },
+};
+
+export const userSlice = createSlice({
+  name: "user",
+  initialState,
+  reducers: {
+    setUser: (state, action) => {
+      state.user = { ...state.user, ...action.payload };
+    },
+  },
+});
+
+/// export User Actions
+export const { setUser } = userSlice.actions;
+
+// export Selectors
+export const selectUser = (state) => state.user.user;
+
+// export UserReducer
+export default userSlice.reducer;
