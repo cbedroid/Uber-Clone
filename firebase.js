e488c4d8e12b895cd192c690b46746d1639d7ceb
cbedroid <cbedroid1614@gmail.com>
2022-03-25 19:05:45 -0400

    Updated firebase config

--- firebase.js
+++ firebase.js
@@ -7,19 +7,21 @@ import { getAuth } from "firebase/auth";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyDW4XX5OPIH2AB24k08SxfL_9Fn4UK0DYQ",
-  authDomain: "uber-clone-342107.firebaseapp.com",
-  projectId: "uber-clone-342107",
-  storageBucket: "uber-clone-342107.appspot.com",
-  messagingSenderId: "556992996945",
-  appId: "1:556992996945:web:c569e48d817b651239ba70",
+export const firebaseConfig = {
+  apiKey: "AIzaSyCCdT8FWzO4ygT5xsZ5ylos_k1GKuwAdT4",
+  authDomain: "uber-clone-ef29f.firebaseapp.com",
+  projectId: "uber-clone-ef29f",
+  storageBucket: "uber-clone-ef29f.appspot.com",
+  messagingSenderId: "629360553355",
+  appId: "1:629360553355:web:edb2a054689cb8f4fb138e",
 };
 
 // Initialize Firebase
 const apps = getApps();
 let app = apps.length === 0 ? initializeApp(firebaseConfig) : getApp();
 
-export const auth = getAuth(app);
+const auth = getAuth(app);
+auth.useDeviceLanguage();
 
 export default app;
+export { auth };
