d27f48bc8e4a25e4d006c51a044ef1da549746ce
cbedroid <cbedroid1614@gmail.com>
2022-03-27 10:33:56 -0400

    Added Firebase provider and Redux app persist

--- src/App.js
+++ src/App.js
@@ -1,36 +1,22 @@
 import React, { useState } from "react";
 import { KeyboardAvoidingView, Platform } from "react-native";
+import { LogBox } from "react-native";
 import { useReduxDevToolsExtension } from "@react-navigation/devtools";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import Apploading from "expo-app-loading";
-import * as Font from "expo-font";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import "react-native-gesture-handler";
 import { Provider } from "react-redux";
-import AccountRegisterScreen from "./screens/Accounts/AccountRegisterScreen";
-import EatsScreen from "./screens/EatsScreen";
-import HomeScreen from "./screens/HomeScreen";
-import LoadingScreen from "./screens/LoadingScreen";
-import MapScreen from "./screens/MapScreen";
-import PickupTimeScreen from "./screens/PickupTimeScreen";
-import SafetyScreen from "./screens/SafetyScreen";
-import SearchScreen from "./screens/SearchScreen";
-import SelectPlaceScreen from "./screens/SelectPlaceScreen";
+import { PersistGate } from "redux-persist/integration/react";
+import { APP_SCREENS } from "./constants/appConstants";
+import { FirebaseProvider } from "./contexts/FirebaseContext";
 import Store from "./store/index";
+import { appPersist } from "./store/index";
+import { loadFont } from "./Utils";
+LogBox.ignoreLogs(["Async Storage has been extracted from react-native core"]);
 
-const loadFont = async () =>
-  Font.loadAsync({
-    UberMoveRegular: require("./assets/fonts/UberMove-Regular.ttf"),
-    UberMoveMedium: require("./assets/fonts/UberMove-Medium.ttf"),
-    UberTextRegular: require("./assets/fonts/UberMoveText-Regular.ttf"),
-    VisbySemibold: require("./assets/fonts/VisbySemibold.ttf"),
-    VisbyRegular: require("./assets/fonts/VisbyRegular.ttf"),
-    VisbyMedium: require("./assets/fonts/VisbyMedium.ttf"),
-    VisbyLight: require("./assets/fonts/VisbyLight.ttf"),
-    VisbyThin: require("./assets/fonts/VisbyThin.ttf"),
-    VisbyThinItalic: require("./assets/fonts/VisbyThinItalic.ttf"),
-  });
+export const APP_NAME = "Uber Clone";
 
 export default function App() {
   const navigationRef = React.createRef();
@@ -39,63 +25,28 @@ export default function App() {
 
   useReduxDevToolsExtension(navigationRef);
 
-  const screens = [
-    {
-      name: "LoadingScreen",
-      component: LoadingScreen,
-    },
-
-    {
-      name: "SafetyScreen",
-      component: SafetyScreen,
-    },
-    {
-      name: "AccountRegisterScreen",
-      component: AccountRegisterScreen,
-    },
-    {
-      name: "HomeScreen",
-      component: HomeScreen,
-    },
-
-    {
-      name: "MapScreen",
-      component: MapScreen,
-    },
-
-    {
-      name: "EatsScreen",
-      component: EatsScreen,
-    },
-
-    {
-      name: "PickupTimeScreen",
-      component: PickupTimeScreen,
-    },
-    {
-      name: "SearchScreen",
-      component: SearchScreen,
-    },
-  ];
-
   if (fontLoaded) {
     return (
       <Provider store={Store}>
-        <NavigationContainer ref={() => navigationRef}>
-          <SafeAreaProvider>
-            <KeyboardAvoidingView
-              behavior={Platform.OS === "ios" ? "padding" : "height"}
-              keyboardVerticalOffset={Platform.OS === "ios" ? -64 : 0}
-              style={{ flex: 1 }}
-            >
-              <Stack.Navigator initialRouteName="LoadingScreen">
-                {screens.map((screen, index) => (
-                  <Stack.Screen key={index} name={screen.name} component={screen.component} animationEnabled={true} options={{ headerShown: false }} />
-                ))}
-              </Stack.Navigator>
-            </KeyboardAvoidingView>
-          </SafeAreaProvider>
-        </NavigationContainer>
+        <PersistGate persistor={appPersist}>
+          <FirebaseProvider>
+            <NavigationContainer ref={() => navigationRef}>
+              <SafeAreaProvider>
+                <KeyboardAvoidingView
+                  behavior={Platform.OS === "ios" ? "padding" : "height"}
+                  keyboardVerticalOffset={Platform.OS === "ios" ? -64 : 0}
+                  style={{ flex: 1 }}
+                >
+                  <Stack.Navigator initialRouteName="LoadingScreen">
+                    {APP_SCREENS.map((screen, index) => (
+                      <Stack.Screen key={index} name={screen.name} component={screen.component} animationEnabled={true} options={{ headerShown: false }} />
+                    ))}
+                  </Stack.Navigator>
+                </KeyboardAvoidingView>
+              </SafeAreaProvider>
+            </NavigationContainer>
+          </FirebaseProvider>
+        </PersistGate>
       </Provider>
     );
   } else {
