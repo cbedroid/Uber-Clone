87d04873b2cc125ba8f5138b50f32115543918bb
cbedroid <cbedroid1614@gmail.com>
2023-01-15 08:54:35 -0500

    Fixed firebase config, added env example

--- src/firebase.js
+++ src/firebase.js
@@ -1,25 +1,26 @@
 /* eslint-disable no-unused-vars */
 // Import the functions you need from the SDKs you need
 // eslint-disable-next-line import/no-unresolved
-import { FIREBASE_APIKEY, FIREBASE_AUTHDOMAIN, FIREBASE_PROJECTID, FIREBASE_STORAGEBUCKET, FIREBASE_MESSAGINGSENDERID, FIREBASE_APPID } from "@env";
-import { initializeApp, getApp, getApps } from "firebase/app";
+// import { process.env.REACT_APP_FIREBASE_APIKEY, process.env.REACT_APP_FIREBASE_AUTHDOMAIN, process.env.REACT_APP_FIREBASE_PROJECTID, process.env.REACT_APP_FIREBASE_STORAGEBUCKET, process.env.REACT_APP_FIREBASE_MESSAGINGSENDERID, process.env.REACT_APP_FIREBASE_APPID } from "@env";
+import { getApp, getApps, initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 export const firebaseConfig = {
-  apiKey: FIREBASE_APIKEY,
-  authDomain: FIREBASE_AUTHDOMAIN,
-  projectId: FIREBASE_PROJECTID,
-  storageBucket: FIREBASE_STORAGEBUCKET,
-  messagingSenderId: FIREBASE_MESSAGINGSENDERID,
-  appId: FIREBASE_APPID,
+  apiKey: process.env.REACT_APP_FIREBASE_APIKEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTHDOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECTID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGEBUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGINGSENDERID,
+  appId: process.env.REACT_APP_FIREBASE_APPID,
 };
 
 // Initialize Firebase
 const apps = getApps();
-let app = apps.length === 0 ? initializeApp(firebaseConfig) : getApp();
+const app = apps.length === 0 ? initializeApp(firebaseConfig) : getApp();
 
 const auth = getAuth(app);
 
